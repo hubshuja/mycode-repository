95e5a31b5adad9d37f7188def827aaabead0a6bd
hubshuja <shujauom@gmail.com>
2020-04-24 18:26:04 +0500

    Add files via upload

---
+++ FrontController.php
@@ -0,0 +1,106 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use TPDF;
+use PDF;
+class FrontController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+        return view('front.home');
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+    public function testPDF(){
+      
+
+// create new PDF document
+// set document information
+   $pdf = PDF::loadView('pdftesting');
+return $pdf->download('invoice.pdf');
+    }
+  public function getPageBySlug(){
+        return view('front.page');
+      
+  }
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+         
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+    public function subScribe(Request $request){
+         $data =  [
+                        'sub_email' =>$request->input('sub_email'),
+                        'created_at' =>date('Y-m-d'),
+                        'updated_at' =>date('Y-m-d'),
+                    ];
+                    \App\Subscription::insert($data);
+    }
+}
